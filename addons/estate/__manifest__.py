f599a984d2fee057d8bff328743a27add02a7d23
Ahmadm-dl <ahmad..m.mustapha@hotmail.com>
2025-10-22 16:54:30 +0300

    Hot Fix

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Estate Management",
+    "version": "1.0",
+    "category": "Real Estate",
+    "summary": "Manage real estate properties, agents, and clients.",
+    "description": "This module provides functionalities to manage real estate properties, agents, and clients. It includes features for property listings, agent assignments, and client interactions.",
+    "author": "Ahmad Mustapha",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
